5fc3dc1c6e0a5335acf5a377cce4ff7436d2fe0e
lks <endygamessk@gmail.com>
2023-03-07 21:49:10 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once('config.php');
+try {
+    $db = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $query = "SELECT * FROM person";
+    $stmt = $db->query($query);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <div class="container-md">
+        <h1>Zadanie 1 - Olympic Games</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <td>Meno</td>
+                    <td>Priezvisko</td>
+                    <td>Narodenie</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                foreach ($results as $result) {
+                    echo "<tr><td>" . $result["name"] . "</td><td>" . $result["surname"] . "</td><td>" . $result["birth_day"] . "</td></tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
